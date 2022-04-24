38e6217bedd0a9d01d94e9092b4a20db47dd4749
Saif-Rahman666 <saf.saif@gmail.com>
2022-04-24 16:19:30 +0600

    typo fixed

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,7 +6,7 @@ export const fetchApi = async(url) => {
     const {data} = await axios.get((url),{
         headers: {
             'X-RapidAPI-Host': 'bayut.p.rapidapi.com',
-            'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API,
+            'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
           }
     });
     return data;
